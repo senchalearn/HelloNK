444d4ee6b6824244e5b8e75a5f24b6933810fc8f
James Pearce <github@tripleodeon.com>
2011-06-20 17:31:16 -0700

    3_simple_ui section

--- HTML/app/app.js
+++ HTML/app/app.js
@@ -1,5 +1,6 @@
 new Ext.Application({
     launch: function () {
+        var timeToolbar = new Ext.Toolbar();
         new Ext.TabPanel({
             fullscreen: true,
             dockedItems: [{
@@ -10,6 +11,49 @@ new Ext.Application({
                 ui:'light'
             },
             items: [{
+                title: 'Alerts',
+                dockedItems: [timeToolbar],
+                layout: {type: 'vbox', align:'center'},
+                items: [
+                    {xtype: 'button', text: 'Alert',
+                        listeners: {tap: function() {
+                            alert('A regular JavaScript alert');
+                        }}
+                    },
+                    {xtype: 'button', text: 'Confirm',
+                        listeners: {tap: function() {
+                            confirm('A regular JavaScript confirmation');
+                        }}
+                    },
+                    {xtype: 'button', text: 'NKAlert', ui: 'confirm',
+                        listeners: {tap: function() {
+                            NKAlert('This is a...', '...NimbleKit alert');
+                        }}
+                    },
+                    {xtype: 'button', text: 'NKConfirm', ui: 'confirm',
+                        listeners: {tap: function() {
+                            NKConfirm(
+                                'This is a...', '...NimbleKit confirmation',
+                                'It rocks!', 'I love it!',
+                                'confirmCallback'
+                            );
+                        }}
+                    },
+                    {xtype: 'button', text: 'NKAlertSheet', ui: 'confirm',
+                        listeners: {tap: function() {
+                            var sheet = new NKAlertSheet();
+                            sheet.init('confirmCallback');
+                            sheet.setTitle("Like this?");
+                            sheet.addButtonWithTitle("It rocks!");
+                            sheet.addButtonWithTitle("I love it!");
+                            sheet.addButtonWithTitle("Cancel");
+                            sheet.setStyle("blacktranslucent");
+                            sheet.setRedButtonIndex(2);
+                            sheet.show()
+                        }}
+                    }
+                ]
+            }, {
                 title: 'Info',
                 html:
                     '<table>' +
@@ -28,5 +72,11 @@ new Ext.Application({
                     '</table>'
             }]
         });
+        
+        window.confirmCallback = function (index) {
+            NKAlert('You clicked...', '...button #' + index);
+        }
+        setInterval(function () {timeToolbar.setTitle(new Date().toLocaleTimeString());}, 1000);
+        
     }
 });
