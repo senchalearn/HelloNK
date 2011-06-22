dc81fe9fcf040bb364146c4e1f094b4a7184aa06
James Pearce <github@tripleodeon.com>
2011-06-22 16:03:54 -0700

    5_web_app_radio section

--- HTML/app/app.js
+++ HTML/app/app.js
@@ -16,6 +16,10 @@ new Ext.Application({
             }
         }
 
+        var radio = new NKInternetPlayer();
+        radio.forceDecoderFormat("aac");
+        
+        var radioToolbar = new Ext.Toolbar({title:'Soma FM'});
         
         var tabPanel = new Ext.TabPanel({
             fullscreen: true,
@@ -33,6 +37,33 @@ new Ext.Application({
                 ui:'light'
             },
             items: [{
+                title: 'Radio',
+                layout: {type: 'vbox', align:'center'},
+                defaults: {xtype: 'button'},
+                dockedItems: [radioToolbar],
+                items: [{
+                    xtype: 'sliderfield',
+                    label: 'Volume',
+                    value: 50,
+                    minValue: 0,
+                    maxValue: 100,
+                    listeners: {drag: function(slider, thumb, value) {
+                        radio.setVolume(value);
+                    }}
+                }, {
+                    text: 'Play',
+                    ui: 'confirm',
+                    listeners: {tap: function() {
+                        radio.playFromURL("http://mp1-gs130.somafm.com:80");
+                    }}
+                }, {
+                    text: 'Stop',
+                    ui: 'decline',
+                    listeners: {tap: function() {
+                        radio.stop();
+                    }}
+                }]
+            }, {
                 title: 'Alerts',
                 dockedItems: [timeToolbar],
                 layout: {type: 'vbox', align:'center'},
@@ -102,6 +133,13 @@ new Ext.Application({
             NKAlert('You clicked...', '...button #' + index);
         }
         setInterval(function () {timeToolbar.setTitle(new Date().toLocaleTimeString());}, 1000);
+
+        setInterval(function () {
+            var meta = radio.getCurrentMetaString().match(/'[^']*'/);
+            if (meta) {
+                radioToolbar.setTitle(meta);
+            }
+        }, 5000);
         
         window.switchTab = function () {
             var currentTab = tabPanel.getActiveItem();
